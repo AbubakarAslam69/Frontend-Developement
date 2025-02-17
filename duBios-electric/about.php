c9ad6919459695683efca21cb42005f0e0e0cbde
Abubakar Aslam <127650749+AbubakarAslam69@users.noreply.github.com>
2025-02-17 09:46:09 +0500

    Add files via upload

---
+++ duBios-electric/about.php
@@ -0,0 +1,173 @@
+<?php include 'include/header.php' ?>
+<title>Demo</title>
+
+</head>
+
+<body>
+    <?php include 'include/menu.php' ?>
+    <!-- <div class="preLoader black">
+        <img src="images/logo.png" alt="img">
+    </div>
+    <div class="preLoader white"></div> -->
+
+    <section class="Main">
+        <div class="banner">
+            <div class="bannerimg">
+                <img src="images/bannner.jpg" alt="">
+                <div class="banner-info">
+                    <h1>About Us</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="About">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-md-5">
+                    <figure class="aboutimg">
+                        <img src="images/abtimg.png" alt="">
+                    </figure>
+                </div>
+                <div class="col-md-7">
+                    <div class="abt">
+                        <h2>About Us<span>DuBois Electric Inc</span></h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        </p>
+                        <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing software like Aldus printer took a
+                            galley of type was into PageMaker including versions of Lorem Ipsum.</p>
+                        <div class="anchor">
+                            <a href="#">Discover More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="Mission">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="mission-info">
+                        <h1>Our Mission</h1>
+                        <p class="mp1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                        <p class="mp2">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus printer took a galley of type was into PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="mission-img">
+                        <img src="images/Mission.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> 
+
+    <section class="Teams">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="info">
+                        <h6>TEAM<span>The Industry’s Real Power Players.</span></h6>
+                        <p>We've have decades of experience living life on the line. We know what's at stake for the
+                            teams in the field and the communities we serve. That's why, whether quickly restoring power
+                            after a devastating storm, building robust power infrastructures, or ensuring safety
+                            equipment...</p>
+                    </div>
+                    <div class="members">
+                        <img src="images/team1.png" class="team1" alt="">
+                        <div class="member-info">
+                            <h5>Matt Moeller<span>CEO & President</span></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore.</p>
+                        </div>
+                    </div>
+                    <div class="members">
+                        <img src="images/team2.png" class="team1" alt="">
+                        <div class="member-info">
+                            <h5>Robert Pinto<span>Chief Operating Officer</span></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="teamimg">
+                        <img src="images/team3.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="Blog">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-md-5">
+                    <div class="binfo">
+                        <h6>BLOG<span>Latest Blog</span></h6>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="banc">
+                        <a href="blog.php">VIEW ALL BLOG</a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper blogSwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="blogbox">
+                            <div class="bimg">
+                                <img src="images/b1.png" class="b1i" alt="">
+                            </div>
+                            <div class="binfo1">
+                                <a href="#">Power Line</a>
+                                <h4>Revolutionary Battery Breakthrough Sparks Excitement in Electrical Services</h4>
+                                <p>A recent breakthrough in battery technology promises longer life and increased
+                                    efficiency, solutions in homes and businesses.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="blogbox">
+                            <div class="bimg">
+                                <img src="images/b2.png" class="b1i" alt="">
+                            </div>
+                            <div class="binfo1">
+                                <a href="#">Power Line</a>
+                                <h4>Revolutionary Battery Breakthrough Sparks Excitement in Electrical Services</h4>
+                                <p>A recent breakthrough in battery technology promises longer life and increased
+                                    efficiency, solutions in homes and businesses.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="blogbox">
+                            <div class="bimg">
+                                <img src="images/b3.png" class="b1i" alt="">
+                            </div>
+                            <div class="binfo1">
+                                <a href="#">Power Line</a>
+                                <h4>Revolutionary Battery Breakthrough Sparks Excitement in Electrical Services</h4>
+                                <p>A recent breakthrough in battery technology promises longer life and increased
+                                    efficiency, solutions in homes and businesses.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+
+
+
+
+
+    <?php include 'include/footer.php' ?>
+</body>
